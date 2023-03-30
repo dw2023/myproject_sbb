d8afff9d2958a91c70e5100d7235beceb8af8420
dw2023 <dwkim126@gmail.com>
2023-03-31 03:21:39 +0900

    2-09 서비스

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -11,10 +11,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-    private final QuestionRepository questionRepository;
+    private final QuestionService questionService;
+
     @GetMapping("/question/list")
     public String list(Model model) {
-        List<Question> questionList = this.questionRepository.findAll();
+        List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList); // Model 객체에 "questionList" 라는 이름으로 값을 저장
         return "question_list";
     }
