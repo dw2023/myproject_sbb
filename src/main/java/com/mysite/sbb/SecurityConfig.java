1aa8c49c3ef1e5031d061e21ca1ba65969cf85b3
dw2023 <dwkim126@gmail.com>
2023-04-03 05:38:28 +0900

    3-07 로그아웃

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -30,6 +30,11 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/user/login")
                 .defaultSuccessUrl("/")
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
             ;
 
         return http.build();
